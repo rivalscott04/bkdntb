a9aabbee2fbf7ef78d80dfaecc9bcd665cd43db2
raditnugros <adit.nugros@gmail.com>
2026-07-28 11:01:59 +0800

    update home page

---
+++ application/views/profil-badan.php
@@ -0,0 +1,273 @@
+            <!--Start breadcrumb area-->
+        <section class="breadcrumb-area" style="background-image: url(<?php echo base_url().'assets/'?>images/resources/kantorbkd.png);">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="inner-content clearfix">
+                           
+                            <div class="breadcrumb-menu float-right">
+                                <ul class="clearfix">
+                                                                    </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--End breadcrumb area-->
+
+ <!--Start Company Overview Area-->
+        <section class="company-overview-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="intro-box clearfix">
+                            <div class="sec-title">
+                              
+                                <div class="title">Jejak Pimpinan<br> <span>BKD Provinsi NTB</span></div>
+                            </div>
+                            <div class="text">
+                                 <p>Badan Kepegawaian Daerah (BKD) Provinsi NTB lahir dari penataan perangkat daerah otonom untuk mengelola aparatur sipil negara, yang kemudian mengalami pemisahan fungsi diklat dan manajemen kepegawaian melalui Peraturan Gubernur NTB Nomor 42 Tahun 2017, guna menjalankan kebijakan manajemen kepegawaian secara profesional. Kantor BKD Provinsi NTB berlokasi di Jalan Pejanggik Nomor 14, Kota Mataram.</p>
+                                <br> <a class="btn-one" href="/tupoksi">Tugas, Pokok & Fungsi BKD Provinsi NTB<span class="flaticon-next"></span></a>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                
+
+            </div>
+        </section>
+        <!--End Company Overview Area-->
+
+
+ <section class="featured-area">
+            <br><br><br>
+            <div class="container about-style3-content">
+                  <div class="row">
+                    <div class="col-xl-12">
+                        <div class="sec-title text-center">
+                            
+                            <p>Profil Singkat BKD Provinsi NTB</p>
+                            <div class="title">Kepala Badan<br>Dari Dar Masa ke Masa</div>
+                            
+                        </div>
+                       
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="project-carousel-v2 owl-carousel owl-theme">
+                            <!--Start single project style1-->
+                            <div class="single-project-style2">
+                                <div class="img-holder">
+                                    <a href="https://www.instagram.com/p/DajYFhKDwkb/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="><img src="<?php echo base_url().'assets/'?>images/konten/1.jpg" alt="Awesome Image"></a>
+                                    
+                                    
+                                </div>
+                            </div>
+                            <!--End single project style1-->
+                            <!--Start single project style1-->
+                            <div class="single-project-style2">
+                                <div class="img-holder">
+                                    <a href="https://www.instagram.com/p/DajYFhKDwkb/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="><img src="<?php echo base_url().'assets/'?>images/konten/2.jpg" alt="Awesome Image"></a>
+                                    
+                                    
+                                </div>
+                            </div>
+                            <!--End single project style1-->
+                            <!--Start single project style1-->
+                            <div class="single-project-style2">
+                                <div class="img-holder">
+                                    <a href="https://www.instagram.com/p/DajYFhKDwkb/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="><img src="<?php echo base_url().'assets/'?>images/konten/3.jpg" alt="Awesome Image"></a>
+                                    
+                                  
+                                </div>
+                            </div>
+                            <!--End single project style1-->
+
+                            <!--Start single project style1-->
+                            <div class="single-project-style2">
+                                <div class="img-holder">
+                                    <a href="https://www.instagram.com/p/DajYFhKDwkb/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="><img src="<?php echo base_url().'assets/'?>images/konten/4.jpg" alt="Awesome Image"></a>
+                                    
+                                   
+                                </div>
+                            </div>
+                            <!--End single project style1-->
+                            <!--Start single project style1-->
+                            <div class="single-project-style2">
+                                <div class="img-holder">
+                                    <a href="https://www.instagram.com/p/DajYFhKDwkb/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="><img src="<?php echo base_url().'assets/'?>images/konten/5.jpg" alt="Awesome Image"></a>
+                                    
+                                   
+                                </div>
+                            </div>
+                            <!--End single project style1-->
+                          
+
+                          
+                      
+                        </div>
+                        <br><center><em>Pembaharuan Data : 02 Juni 2026</em></center>
+                    </div>
+                </div>
+
+
+
+            </div>
+
+              
+        </section>
+
+
+
+         <!--Start Testimonial Style3 Area-->
+		 <section class="testimonial-style3-area">
+            <div class="container">
+                <div class="sec-title with-text max-width text-center wow fadeInDown" data-wow-delay="100ms" data-wow-duration="1200ms">
+                    <p>BKD Provinsi NTB</p>
+                    <div class="title">Inspirasi <span>Pagi</span></div>
+                    <p class="bottom-text">Merupakan kata-kata inspirasi yang membangun semangat kerja ASN BKD NTB sesaat setelah mengikuti apel pagi</p>
+                </div>
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12">
+                        <div class="testimonial-carousel-2 owl-carousel owl-theme">
+                            <!--Start Single Testimonial style2-->
+                            <div class="single-testimonial-style3">
+                                <div class="inner-content">
+                                    <div class="client-info">
+                                        
+                                       
+                                            <h3>Lalu Rivaliqa Payanta</h3>
+                                            <span>Assesor SDM UPPK BKD NTB</span>
+                                    
+                                    </div>
+                                    <div class="text-box">
+                                        <p>"Hidup ini bukan tentang masa lalu, masa kini dan masa depan. Tetapi kita bisa belajar dari masa lalu untuk menjadi lebih baik hari ini, sementara masa depan itu adalah hal yang belum pasti"</p>
+                                    </div>
+                                    <div class="review-box">
+                                        <ul>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--End Single Testimonial style2 -->
+
+                            <!--Start Single Testimonial style2-->
+                            <div class="single-testimonial-style3">
+                                <div class="inner-content">
+                                    <div class="client-info">
+                                        
+                                       
+                                            <h3>Ihdiyat Rea Bustani</h3>
+                                            <span>Jafung Bidang Evaluasi</span>
+                                    
+                                    </div>
+                                    <div class="text-box">
+                                        <p>"Teruslah berinovasi dan bekerja. Jadilah bagian dari solusi, bukan bagian dari masalahs"</p>
+                                    </div>
+                                    <div class="review-box">
+                                        <ul>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--End Single Testimonial style2 -->
+                             
+                           <!--Start Single Testimonial style2-->
+                           <div class="single-testimonial-style3">
+                                <div class="inner-content">
+                                    <div class="client-info">
+                                        
+                                       
+                                            <h3>Astri Aditya Graha</h3>
+                                            <span>UPT UPPK BKD NTB</span>
+                                    
+                                    </div>
+                                    <div class="text-box">
+                                        <p>"Tak kan selamanya raga ini menjagamu. Tak kan selamanya tangan ini mendekapmu. Tak ada yang abadi"</p>
+                                    </div>
+                                    <div class="review-box">
+                                        <ul>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--End Single Testimonial style2 -->
+                            <!--Start Single Testimonial style2-->
+                            <div class="single-testimonial-style3">
+                                <div class="inner-content">
+                                    <div class="client-info">
+                                        
+                                       
+                                            <h3>Rosadi Ansori</h3>
+                                            <span>Penelaah Teknis Kebijakan</span>
+                                    
+                                    </div>
+                                    <div class="text-box">
+                                        <p>"Berusahalah agar kita tidak menjadi orang yang sukses. Tetapi berusahalah juga agar kita menjadi orang yang bernilai"</p>
+                                    </div>
+                                    <div class="review-box">
+                                        <ul>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--End Single Testimonial style2 -->
+                            <!--Start Single Testimonial style2-->
+                            <div class="single-testimonial-style3">
+                                <div class="inner-content">
+                                    <div class="client-info">
+                                        
+                                       
+                                            <h3>Moh. Asyik Hidayat</h3>
+                                            <span>Kasubag Umum & Kepegawaian</span>
+                                    
+                                    </div>
+                                    <div class="text-box">
+                                        <p>"Hidup tidak semata-mata tentang menjadi yang terbaik, namun jadilah baik tanpa harus berpura-pura baik"</p>
+                                    </div>
+                                    <div class="review-box">
+                                        <ul>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                            <li><i class="fa fa-star"></i></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--End Single Testimonial style2 -->
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+             <center><a class="btn-one" href="/inspirasi">Inspirsi Pagi Selengkapnya<span class="flaticon-next"></span></a></center>
+        </section>
+        <!--End Testimonial Style3 Area-->
+		
+		<br>
